1e90c881db88f1777be61d71840623cf4ce1c43f
Achal Dixit <43511383+JARACH-209@users.noreply.github.com>
2020-10-08 10:15:32 +0530

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -3,26 +3,17 @@ import math
 #DO NOT MAKE UNNECESSARY CHANGES
 class DistanceFuncs:
 	def calc_distance(self, point_a: List[float], point_b: List[float], dist_func: Callable)->float:
-	""" Calculates distance between two points using the passed dist_func """
 		return dist_func(point_a, point_b)
 	@staticmethod
-	def euclidean(point_a: List[float], point_b: List[float],/)->float:
-"""
-Calculates Euclidean Distance between two points Example:
->>> DistanceFuncs.euclidean([1,1],[1,1])
-0.0
-"""
+	def euclidean(point_a: List[float], point_b: List[float])->float:
 		return math.dist(point_a, point_b)
 	@staticmethod
-	def manhattan_distance(point_a: List[float], point_b: List[float]): """Compute
-	the manhattan_distance between two points""" 
+	def manhattan_distance(point_a: List[float], point_b: List[float]):
 		raise NotImplementedError()
 	@staticmethod
-	def jaccard_distance(point_a: List[float], point_b: List[float]): """Compute
-the jaccard_distance between two points""" 
+	def jaccard_distance(point_a: List[float], point_b: List[float]):
 		raise NotImplementedError()
 defmain():
-"""Demonstrate the usage of DistanceFuncs """
 	test = DistanceFuncs()
 	test.calc_distance([2,3],[10,10])
 	print("The distance is : ",test.euclidean())
